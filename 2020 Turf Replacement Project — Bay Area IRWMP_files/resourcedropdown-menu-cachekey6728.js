9ce72fd35ef257ee52644cded61ea052204339e6
Joseph <jlikhuva@stanford.edu>
2016-12-18 00:56:26 -0800

    Started Monterey. Need to finish BayArea

--- 2020 Turf Replacement Project — Bay Area IRWMP_files/resourcedropdown-menu-cachekey6728.js
+++
@@ -1,4 +0,0 @@
-
-/* - ++resource++dropdown-menu.js - */
-// http://bairwmp.org/portal_javascripts/++resource++dropdown-menu.js?original=1
-jQuery(function($){$('#portal-globalnav .noClick').click(function(e){e.preventDefault()})});
